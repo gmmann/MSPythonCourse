acf1727f0e0386b40de102ce71bf4aeae553aac4
George Mann <george.m.mann@gmail.com>
2019-10-08 09:37:47 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -63,3 +63,49 @@ for x in range(0,10):
 print('loop completed')
 print(datetime.now())
 print()
+
+
+def print_time(task_name):
+    print(task_name)
+    print(datetime.now)
+    print()
+first_name = 'Susan'
+print_time('first name assigned with task')
+
+for x in range(0,10):
+    print(x)
+print_time('loop completed with task')
+print()
+
+
+
+
+
+# first_name = input('Enter your first name: ')
+# first_name_iniital = first_name[0:1]
+# last_name = input('Enter your last name: ')
+# last_name_iniital = last_name[0:1]
+
+# print('Your initials are: ' + first_name_iniital + last_name_iniital)
+
+
+
+def get_initial(name):
+    initial = name[0:1].upper()
+    return initial
+
+first_name = input('Enter your first name: ')
+last_name = input('Enter your last name: ')
+
+first_name_iniital = get_initial(first_name)
+last_name_iniital = get_initial(last_name)
+print('Your initials are: ' + first_name_iniital + last_name_iniital)
+print()
+print('Your initials are via function: ' + get_initial(first_name) + get_initial(last_name))
+print()
+
+
+
+
+
+
